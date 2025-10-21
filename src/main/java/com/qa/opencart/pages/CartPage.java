12eef9a35214cba72ef67670ec05caf366888d61
Utkarsh Patil <utkarshpatil26.com>
2025-10-21 17:19:19 +0530

    adding something into the cart page

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -3,4 +3,15 @@ package com.qa.opencart.pages;
 public class CartPage {
 
 	int i=10;
+	
+	static String name="Test";
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Cart Page");
+		System.out.println(name);
+		
+		//Why a I making changes here?
+		//To check git pull functionality
+	}
 }
